8cbb545db27cb19acf8ae1809c5a031f12e82ad7
Stuart Clark <>
2020-11-08 12:32:05 +0000

    iOS 13 version of the test app - app delegate and the UIView have been updated

--- SimpleSingleFetch/Views/UserView.swift
+++ SimpleSingleFetch/Views/UserView.swift
@@ -13,24 +13,44 @@ struct UserView: View {
     
     var body: some View {
         Group {
-            
-            switch viewModel.viewData.viewState {
-            case .loading:
-                Text("Loading")
-            case .data:
-                List {
-                    ForEach(viewModel.viewData.users) { user in
-                        UserItem(user: user)
-                    }
-                }
-            case .noData:
-                Text("No data to display")
-            case .error:
-                Text("Data error")
-            }
+            body(for: viewModel.viewData.viewState)
+
+// Note iOS 14 makes this so much nicer as we can directly switch on the enum
+//            switch viewModel.viewData.viewState {
+//            case .loading:
+//                Text("Loading")
+//            case .data:
+//                List {
+//                    ForEach(viewModel.viewData.users) { user in
+//                        UserItem(user: user)
+//                    }
+//                }
+//            case .noData:
+//                Text("No data to display")
+//            case .error:
+//                Text("Data error")
+//            }
         }
         .onAppear {
-            viewModel.loadUsers()
+            self.viewModel.loadUsers()
+        }
+    }
+    
+    @ViewBuilder
+    private func body(for state: ViewState) -> some View {
+        
+        if state == .loading {
+            Text("Loading")
+        } else if state ==  .data {
+            List {
+                ForEach(viewModel.viewData.users) { user in
+                    UserItem(user: user)
+                }
+            }
+        } else if state == .noData {
+            Text("No data to display")
+        } else if state == .error {
+            Text("Data error")
         }
     }
 }
